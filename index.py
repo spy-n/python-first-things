3846037fab347e95bfa914e0444e0e12c44fe163
spy-n <76927919+spy-n@users.noreply.github.com>
2021-03-05 15:09:53 -0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,9 @@
+#aca imprimis/printeas Hello world
+print('Hello world')
+# ; en js se ponen las ; para dar siguiente aca se usan para dar dos comandos en una misma linea pero la consola la devuelve como dos lineas nuevas
+#cada funcion aca se llama instruciones
+#como hacer una variable, los nombres sin espacios solo _ de separacion
+mi_variable="Hello world"
+#para ejecutar la variable simplemente se pone el nombre de la variable en este caso mi_variable mas un print para imprimir si no no se mostrara nada
+print(mi_variable)
+#retorna hello world
